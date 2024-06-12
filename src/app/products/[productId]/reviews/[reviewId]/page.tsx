0ee6b0bcd5db3a7eb9939fee208e514a388fbf4c
Rahul Ghimire <ghimirerahul554@gmail.com>
2024-06-12 08:42:50 +0545

    changes

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 export default function Page({
   params,
 }: {
@@ -6,6 +7,9 @@ export default function Page({
     productId: "string";
   };
 }) {
+  if (parseInt(params?.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <div>
       This is a reveiew page of product {params.productId} & review{" "}
